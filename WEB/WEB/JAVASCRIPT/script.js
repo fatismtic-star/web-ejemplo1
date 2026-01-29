fd4023f6dac897419fa53010a9f567577af325d0
fatismtic-star <fati.smtic@gmail.com>
2026-01-29 09:38:07 -0600

    Add files via upload

---
+++ WEB/WEB/JAVASCRIPT/script.js
@@ -0,0 +1,7 @@
+﻿//obtener el boton por su ID que se puso
+const boton1=document.getElementById('love');
+
+//Agregar un evento al boton
+boton1.addEventListener('click', function(){
+     alert("YEAHH");
+});
